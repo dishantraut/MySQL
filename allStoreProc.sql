4bee2b8681975a687e8995954060bd9bba08b3a3
dishantraut <63045109+dishantraut@users.noreply.github.com>
2022-10-19 13:18:49 +0530

    Update allStoreProc.sql

--- allStoreProc.sql
+++ allStoreProc.sql
@@ -442,3 +442,170 @@ CALL `AcquirerSide_OriginalDeclineReversalSuccess`('AcqRevSuccess', 'AcqOgDeclin
 CALL `AcquirerSide_OriginalDeclineReversalSuccess`('AcqOgDecline', 'AcqOgDecline', 'AcqRevSuccess');
 #DROP PROCEDURE IF EXISTS `AcquirerSide_OriginalDeclineReversalSuccess`;
 
+
+-- ################################################################################################### 
+-- * ######################## CCX30/CCX24 :- GetFinalAmtFromIdp ########################## 
+-- ################################################################################################### 
+
+DROP PROCEDURE IF EXISTS GetFinalAmtFromIdp;
+DELIMITER $$
+CREATE PROCEDURE GetFinalAmtFromIdp(IN idp VARCHAR(40), OUT amtBC INT(20), OUT amtAC INT(20), OUT commAmt INT(20))
+COMMENT '
+# Func Name :- GetFinalAmtFromIdp
+# Created By :- Dishant Raut [15Sep2022]
+# Updated By :- Dishant Raut [15Sep2022]
+# Params :- IDP (20)
+# Desc :- Get Amount Before Comm / Total Comm / After Comm Based On IDP
+# Exec :- SELECT GetFinalAmtFromIdp("IDP001NAN00000000000");
+# Drop :- DROP FUNCTION IF EXISTS GetFinalAmtFromIdp;
+'
+BEGIN
+
+    SET @PurBC = @PurAC = @PurCom = 1;
+    SET @Pur = CONCAT('SELECT IFNULL(SUM(TransactionAmount), 0) , IFNULL(SUM(FinalAmount), 0), IFNULL(SUM(TotalCommissions), 0) INTO @PurBC, @PurAC, @PurCom FROM fulldaytransactionhistory WHERE MessageType = "0210" AND ProcessingCode = "000000" AND ResponseCode = "00" AND IDP = "', idp, '";');
+    PREPARE statement1_ FROM @Pur;
+    EXECUTE statement1_;
+
+    SET @RefRevBC = @RefRevAC = @RefRevCom = 1;    
+    SET @RefRev = CONCAT('SELECT IFNULL(SUM(TransactionAmount), 0), IFNULL(SUM(FinalAmount), 0), IFNULL(SUM(TotalCommissions), 0) INTO @RefRevBC, @RefRevAC, @RefRevCom FROM fulldaytransactionhistory WHERE MessageType = "0420" AND ProcessingCode = "200000" AND ResponseCode = "00" AND IDP = "', idp, '";');
+    PREPARE statement2_ FROM @RefRev;
+    EXECUTE statement2_;
+
+    SET @RefBC = @RefAC = @RefCom = 1;
+    SET @Ref = CONCAT('SELECT IFNULL(SUM(TransactionAmount), 0), IFNULL(SUM(FinalAmount), 0), IFNULL(SUM(TotalCommissions), 0) INTO @RefBC, @RefAC, @RefCom  FROM fulldaytransactionhistory WHERE MessageType = "0210" AND ProcessingCode = "200000" AND ResponseCode = "00" AND IDP = "', idp, '";');
+    PREPARE statement3_ FROM @Ref;
+    EXECUTE statement3_;
+    
+    SET @RevBC = @RevAC = @RevCom = 1;
+    SET @Rev = CONCAT('SELECT IFNULL(SUM(TransactionAmount), 0), IFNULL(SUM(FinalAmount), 0), IFNULL(SUM(TotalCommissions), 0) INTO @RevBC, @RevAC, @RevCom FROM fulldaytransactionhistory WHERE MessageType = "0420" AND ProcessingCode = "000000" AND ResponseCode = "00" AND IDP = "', idp, '";');
+    PREPARE statement4_ FROM @Rev;
+    EXECUTE statement4_;
+    
+    SET amtBC = (@PurBC+@RefRevBC)-(@RefBC+@RevBC);
+    SET amtAC = (@PurAC+@RefRevAC)-(@RefAC+@RevAC);
+    SET commAmt = (@PurCom+@RefRevCom)-(@RefCom+@RevCom);
+
+END$$
+DELIMITER ;
+CALL GetFinalAmtFromIdp('IDP002NAN15214307a70', @amtBc, @amtAc);
+SELECT @amtBc;
+SELECT @amtAc;  
+SELECT @commAmt;  
+-- DROP PROCEDURE IF EXISTS GetFinalAmtFromIdp;
+
+
+-- ################################################################################################### 
+-- * ######################## PhaseOne_MissingAtAcquirerExtract ########################## 
+-- ################################################################################################### 
+DROP PROCEDURE IF EXISTS PhaseOne_MissingAtAcquirerExtract;
+DELIMITER $$
+CREATE PROCEDURE PhaseOne_MissingAtAcquirerExtract()
+COMMENT '
+# Proc Name :- PhaseOne_MissingAtAcquirerExtract
+# Created By :- Ankita Harad [26Sep2022]
+# Updated By :- Ankita Harad [26Sep2022]
+# Params :- No Params Required
+# Desc :- Get All Issuer Side Missing Trnx
+            1. Issuer Side More Trnx
+            2. Acquirer Side Less Trnx
+# Exec :- CALL PhaseOne_MissingAtAcquirerExtract();
+# Drop :- DROP PROCEDURE IF EXISTS PhaseOne_MissingAtAcquirerExtract;
+'
+BEGIN
+-- * Issuer Side More Acquirer Side Missing
+SELECT
+    ForwardingInstitutionIdentification AS Brd,
+    AcquiringInstitutionIdentification AS Swt,
+    Track2Data AS CardNo,
+    SystemsTraceAuditNumber AS TcId,
+    MessageType AS MT,
+    ProcessingCode AS PC,
+    ResponseCode AS RC,
+    PrimaryMessageAuthenticationCodeMAC AS DI,
+    CardType AS DC,
+    RetrievalReferenceNumber AS RRN,
+    TransactionAmount AS TAmt,
+    ChannelType AS Chn,
+    CardAcceptorIdentification AS RetId,
+    PrimaryAccountNumberPAN AS Bin,
+    LocalTransactionDate AS LDate,
+    LocalTransactionTime AS LTime,
+    BrandName(ForwardingInstitutionIdentification) AS BN,
+    ResponseCodeDescription(ResponseCode) AS RC,
+    SwitchName(AcquiringInstitutionIdentification, ChannelType) AS SwitchName,
+    TransactionType(MessageType, ProcessingCode) AS TrxTyp
+FROM
+    issuerextract
+WHERE
+    ResponseCode = "00"
+    AND RetrievalReferenceNumber NOT IN (
+        SELECT
+            RetrievalReferenceNumber
+        FROM
+            acquirerextract
+        WHERE
+            ResponseCode = "00"
+    );
+END$$
+DELIMITER ;
+CALL PhaseOne_MissingAtAcquirerExtract;
+#DROP PROCEDURE IF EXISTS PhaseOne_MissingAtAcquirerExtract;
+
+
+-- ################################################################################################### 
+-- * ######################## PhaseOne_MissingAtIssuerExtract ########################## 
+-- ################################################################################################### 
+DROP PROCEDURE IF EXISTS PhaseOne_MissingAtIssuerExtract;
+DELIMITER $$
+CREATE PROCEDURE PhaseOne_MissingAtIssuerExtract()
+COMMENT '
+# Proc Name :- PhaseOne_MissingAtIssuerExtract
+# Created By :- Ankita Harad [26Sep2022]
+# Updated By :- Ankita Harad [26Sep2022]
+# Params :- No Params Required
+# Desc :- Get All Acquirer Side Missing Trnx
+            1. Issuer Side Less Trnx
+            2. Acquirer Side More Trnx
+# Exec :- CALL PhaseOne_MissingAtIssuerExtract();
+# Drop :- DROP PROCEDURE IF EXISTS PhaseOne_MissingAtIssuerExtract;
+'
+BEGIN
+-- Acquirer Side More Issuer Side Missing
+SELECT
+    ForwardingInstitutionIdentification AS Brd,
+    AcquiringInstitutionIdentification AS Swt,
+    Track2Data AS CardNo,
+    SystemsTraceAuditNumber AS TcId,
+    MessageType AS MT,
+    ProcessingCode AS PC,
+    ResponseCode AS RC,
+    PrimaryMessageAuthenticationCodeMAC AS DI,
+    CardType AS DC,
+    RetrievalReferenceNumber AS RRN,
+    TransactionAmount AS TAmt,
+    ChannelType AS Chn,
+    CardAcceptorIdentification AS RetId,
+    PrimaryAccountNumberPAN AS Bin,
+    LocalTransactionDate AS LDate,
+    LocalTransactionTime AS LTime,
+    BrandName(ForwardingInstitutionIdentification) AS BN,
+    ResponseCodeDescription(ResponseCode) AS RC,
+    SwitchName(AcquiringInstitutionIdentification, ChannelType) AS SwitchName,
+    TransactionType(MessageType, ProcessingCode) AS TrxTyp
+FROM
+    acquirerextract
+WHERE
+    ResponseCode = "00"
+    AND RetrievalReferenceNumber NOT IN (
+        SELECT
+            RetrievalReferenceNumber
+        FROM
+            issuerextract
+        WHERE
+            ResponseCode = "00"
+    );
+END$$
+DELIMITER ;
+CALL PhaseOne_MissingAtIssuerExtract;
+#DROP PROCEDURE IF EXISTS PhaseOne_MissingAtIssuerExtract;
+
